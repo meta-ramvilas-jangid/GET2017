2eea6ff3b4ee8e06212b594e7ad598646148b008
meta-ramvilas-jangid <ramvilas.jangid@metacube.com>
2017-07-17 17:23:20 +0530

    Update Order.java

--- pfsession1/src/Order.java
+++ pfsession1/src/Order.java
@@ -1,7 +1,13 @@
 public class Order {
-	int checkOrder(int []arr) {
+	int checkOrder(int[] arr) {
+		if(arr==null){
+		    throw new NullPointerException();
+		}
 		int status=0;
-		if(arr.length==0){
+		/**
+		 * if no. of elements are 1 or 0 then return no order
+		 */
+		if(arr.length==0 || arr.length==1){
 			status=0;
 		}else{
 			int flag=arr[1]-arr[0];
@@ -10,8 +16,8 @@ public class Order {
 			}else if(flag<0){
 				status=2;
 			}
-			for(int i=2;i<arr.length;i++){
-				if((arr[i-1] < arr[i] && status==2)  || (arr[i-1] > arr[i] && status==1 )  ){
+			for(int i=2; i<arr.length; i++){
+				if((arr[i-1] < arr[i] && status==2)  || (arr[i-1] > arr[i] && status==1 )){
 					status=0;
 					break;
 				}else if(arr[i-1] < arr[i] &&status==0){
@@ -21,8 +27,6 @@ public class Order {
 				}
 			}
 		}
-		
 		return status;
 	}
-
 }
