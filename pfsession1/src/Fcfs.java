51529f2d3e5ed6e53d653338ef6fc4ee006f2a2f
meta-ramvilas-jangid <ramvilas.jangid@metacube.com>
2017-07-17 17:20:29 +0530

    Update Fcfs.java

--- pfsession1/src/Fcfs.java
+++ pfsession1/src/Fcfs.java
@@ -1,46 +1,40 @@
 public class Fcfs {
-	int[][] fcfs(int[] arr,int[] size)
-	{
-		int n=arr.length;
-		int wait[]=new int[n];
-		int finish[]=new int[n];
-		int start[]=new int[n];
+	/**
+	 * @param arrival_time
+	 * @param Job_size
+	 * @return output that contain s.no,arrival_time,waiting_time,start_time,finish_time 
+	 */
+	int[][] fcfs(int[] arrival_time, int[] Job_size){
+		int wait[]=new int[arrival_time.length];                       
+		int finish[]=new int[arrival_time.length];
+		int start[]=new int[arrival_time.length];
 
-		wait[0]=0;
-	    start[0]=arr[0];
-	    finish[0]= size[0];
-	    for(int i=1;i<n;i++)
+		wait[0]=0;                                          //first process waiting time is 0
+	    start[0]=arrival_time[0];
+	    finish[0]= Job_size[0];
+	    for(int i=1;i<arrival_time.length;i++)
 		{
-			wait[i]=finish[i-1]-arr[i]+1;
-			if(wait[i]<0)
-			{
+			wait[i]=finish[i-1]-arrival_time[i]+1;      //waiting time of ith is finish time of 
+			                                            //(i-1)th -arrival time of ith process 
+			if(wait[i]<0){                               //if waiting time is negative then wait[i]=0
 				wait[i]=0;
 			}
-			if(wait[i]==0)
-			{
-				start[i]=arr[i];
+			if(wait[i]==0){
+			
+				start[i]=arrival_time[i];              //ith start time is (i-1)th finish time+1
 			}else{
 				start[i]=finish[i-1]+1;
 			}
-		    finish[i]=start[i]+size[i]-1;	
+		    finish[i]=start[i]+Job_size[i]-1;	
 	    }
-	    int[][] output =new int[n][5];
-	    for(int i=0;i<n;i++)
+	    int[][] output =new int[arrival_time.length][5];
+	    for(int i=0;i<arrival_time.length;i++)
 	    {
 	    	output[i][0]=i+1;
-	    	output[i][1]=arr[i];
+	    	output[i][1]=arrival_time[i];
 	    	output[i][2]=wait[i];
 	    	output[i][3]=start[i];
 	    	output[i][4]=finish[i];
-	    }
-	    
-	    for(int i=0;i<n;i++)
-	    {
-	    	for(int j=0;j<5;j++)
-	    	{
-	    		System.out.print("  "+output[i][j]+"  ");
-	    	}
-	    	System.out.println();	    	
 	    }
 	    return output;
 	}
