17ea9572e5139674599027e082176d16e2f821e1
meta-ramvilas-jangid <ramvilas.jangid@metacube.com>
2017-07-18 11:59:39 +0530

    Update Pattern2.java

--- PF2Assignment/src/Pattern2.java
+++ PF2Assignment/src/Pattern2.java
@@ -1,5 +1,14 @@
 public class Pattern2 {
+	/**
+	 * space method take two arguments
+	 * @param row particular row in pattern
+	 * @param n maximum numeric value
+	 * @return string of space
+	 */
 	String space(int row, int n){
+		if(row>n){
+    		       return null;    //particular row not present in this pattern
+    	        }
 		String pattern="";	
 		for(int j=0; j<(row-1); j++)
 			{
@@ -7,7 +16,16 @@ public class Pattern2 {
 			}
 		return pattern;
 	}
+	/**
+	 * numeric function take 2 arguments
+	 * @param row particular row in pattern
+	 * @param n maximum numeric value in pyramind
+	 * @return String of numbers
+	 */
 	String number(int row, int n){
+		if(row>n){
+    		        return null;    //particular row not present in this pattern
+    	        }
 		String pattern="";
 		int count=1;
 		for(int j=0; j<=(n-row);j++){
@@ -16,6 +34,11 @@ public class Pattern2 {
 		}
 		return pattern;
 	}
+	/**
+        * print function take one argument
+        * @param n maximum numeric value of pattern
+        * @return String array that contain every row of pattern
+        */
 	String[] print(int n){
     	String pattern[]=new String[n];
     	for(int i=1; i<=n; i++){
