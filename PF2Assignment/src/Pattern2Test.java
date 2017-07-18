1d5ccabeed563aeb4f7b44756abcf284b9aeb709
meta-ramvilas-jangid <ramvilas.jangid@metacube.com>
2017-07-18 12:01:40 +0530

    Update Pattern2Test.java

--- PF2Assignment/src/Pattern2Test.java
+++ PF2Assignment/src/Pattern2Test.java
@@ -1,27 +1,21 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 public class Pattern2Test {
-
 	@Test
 	public void testSpace1() {
 		Pattern2 p1=new Pattern2();
 		String test1=p1.space(1, 5);
 		assertEquals("", test1);
 	}
-
-	@Test
+        @Test
 	public void testNumber1() {
-		 Pattern2 p1=new Pattern2();
-		   String test1=p1.number(1, 5);
-		   assertEquals("12345", test1);
+	  	  Pattern2 p1=new Pattern2();
+		  String test1=p1.number(1, 5);
+		  assertEquals("12345", test1);
 	}
-
 	@Test
 	public void testPrint1() {
-
-		 Pattern2 p1=new Pattern2();
+          	 Pattern2 p1=new Pattern2();
 		 String test1[]=p1.print(5);
 		 String excepted[]={"12345",
 				            " 1234",
@@ -30,26 +24,21 @@ public class Pattern2Test {
 				            "    1"};
 		 assertArrayEquals(excepted, test1);
 	}
-
-	@Test
+        @Test
 	public void testSpace2() {
-
-		   Pattern2 p1=new Pattern2();
+                   Pattern2 p1=new Pattern2();
 		   String test1=p1.space(2, 4);
 		   assertEquals(" ", test1);
 	}
-
-	@Test
+        @Test
 	public void testNumber2() {
 		   Pattern2 p1=new Pattern2();
 		   String test1=p1.number(2, 4);
 		   assertEquals("123", test1);
 	}
-
-	@Test
+        @Test
 	public void testPrint2() {
-
-		 Pattern2 p1=new Pattern2();
+                 Pattern2 p1=new Pattern2();
 		 String test1[]=p1.print(4);
 		 String excepted[]={"1234",
 				            " 123",
@@ -71,9 +60,7 @@ public class Pattern2Test {
 		   Pattern2 p1=new Pattern2();
 		   String test1=p1.number(3, 5);
 		   assertEquals("123", test1);
-
-	}
-
+        }
 	@Test
 	public void testPrint3() {
 		Pattern2 p1=new Pattern2();
@@ -85,7 +72,5 @@ public class Pattern2Test {
 				            "    12",
 				            "     1"}; 
 		 assertArrayEquals(excepted, test1);
-	}
-
-	
+	}	
 }
