a6b223a6f8d33a448405a38f0c520bd010e4637e
meta-ramvilas-jangid <ramvilas.jangid@metacube.com>
2017-07-27 08:32:26 +0530

    Update ShoppingStoreTerminal.java

--- assignmentOopDay7/src/assignmentOopDay7/ShoppingStoreTerminal.java
+++ assignmentOopDay7/src/assignmentOopDay7/ShoppingStoreTerminal.java
@@ -1,14 +1,49 @@
 package assignmentOopDay7;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Set;
 
 public class ShoppingStoreTerminal {
-	static Map<Product, Integer> orderMap;
+	private Map<Product, Integer> orderMap; // to retain the map of orders
+	private double totalAmount; // total amount for order
+	private double totalProductDiscount; // total discount for order
+	private double netProductAmount; // total product discount
+	private double totalOrderDiscount; // total order discount
 
+	/**
+	 * constructor
+	 */
+	public ShoppingStoreTerminal() {
+		totalAmount = 0;
+		totalProductDiscount = 0;
+		netProductAmount = 0;
+		totalOrderDiscount = 0;
+	}
+	/**
+	 * to print the final bill
+	 */
+	public void printFinalReceipt() {
+		double finalAmount = 0;
+		double finalDiscount = 0;
+		DecimalFormat decimaFormat = new DecimalFormat("00.00");
+		System.out.println("#######################################");
+		System.out.println("Sub Total Rs" + totalAmount);
+		System.out.println("Product Discount Total Rs" + totalProductDiscount);
+		System.out.println("Order Discount Total Rs" + totalOrderDiscount);
+		finalDiscount = totalProductDiscount + totalOrderDiscount;
+		System.out.println("Total Discounts Rs" + decimaFormat.format(finalDiscount));
+		finalAmount = totalAmount - finalDiscount;
+		System.out.println("Total: Rs" + decimaFormat.format(finalAmount));
+	}
+	/**
+	 * main method initializes warehouse then product promos the order promos
+	 * initializes order ask the user for input calculates discount print
+	 * receipt
+	 */
 	public static void main(String[] args) throws IOException {
+		ShoppingStoreTerminal sst = new ShoppingStoreTerminal();
 		WareHouse w = new WareHouse();
 		w.displayProducts();
 		ProductLevelPromotions productPromos = new ProductLevelPromotions();
@@ -16,92 +51,15 @@ public class ShoppingStoreTerminal {
 		OrderLevelPromotions orderPromo = new OrderLevelPromotions();
 		orderPromo.displayOrderLevelPromo();
 		Order order = new Order();
-		System.out.println("how many products to buy");
-		@SuppressWarnings("resource")
-		Scanner sc = new Scanner(System.in);
-		int quantity = sc.nextInt();
-		String productId;
-		int enteredQuantity;
-
-		while (quantity > 0) {
-			System.out.println("enter product code from available products");
-			productId = sc.next();
-			if (w.getProductById(productId) == null) {
-				System.out.println("Product Id doesn't exist pls retype ");
-				continue;
-			}
-			System.out.println("Quantity :");
-			enteredQuantity = sc.nextInt();
-
-			if (enteredQuantity > quantity) {
-				System.out.println("invalid quantity ,retype quantity");
-				continue;
-			}
-			order.addProduct(w.getProductById(productId), enteredQuantity);
-			quantity = quantity - enteredQuantity;
-
-		}
-
-		System.out.println("///////////////");
-		
+		order.askAndValidateOrder(w);
 		order.displayOrder();
 
-		/////////////////////////////////////////////////////////////////////////////
-	
-
-		//////////////////////////////////////////////////////////////////////////////
-		
-		System.out.println("APPLIED PROMOTIONS/DISCOUNTS :");
-	
-		orderMap = order.getOrderMap();
-		@SuppressWarnings("unused")
-		double totalProductDiscount = 0;
-		double totalAmount = 0;
-		double prductLevelDiscount=0;
-		Set<Product> products = orderMap.keySet();
-		for (Product pro : products){
-			System.out.print("Promotion : ");
-			double productDiscount = 0;
-			for (ProductLevelPromotions p : productPromos.productLevel){
-					double amount = 0;
-					
-						
-				
-						if (p.isApplicable(pro.getId())) {
-							amount = pro.getPrice();
-							productDiscount = p.getDiscount(amount, p.discount);
-							amount = amount * orderMap.get(pro);
-							productDiscount = productDiscount * orderMap.get(pro);
-							System.out.println(productDiscount+" off on"+pro.getName()+" [Code:"+pro.getId()+"]");
-							amount = amount - productDiscount;
-							System.out.println("Discount : "+productDiscount);
-							prductLevelDiscount+=productDiscount;
-						}
-					totalAmount += amount;
-				
-				//	System.out.println("--------------------------- amount : "+totalAmount);
-				}
-				//System.out.println("Discount : "+productDiscount);
-		}
-		System.out.println("\n\nprductLevelDiscount "+prductLevelDiscount);
-		System.out.println("\n\n\ntotal Amout : " + totalAmount);
-		
-		//////////////////////////////////////////////////////////////////
-		double maxDiscount = 0;
-		for (OrderLevelPromotions o : orderPromo.orderLevel) {
-			double discount = 0;
-			if (o.isApplicable(totalAmount + "") && totalAmount >= o.discountOnAmount) {
-				discount = o.getDiscount(totalAmount, o.discount);
-				if (discount > maxDiscount) {
-					maxDiscount = discount;
-				}
-			}
-		}
-		System.out.println("order Level Discount : " + maxDiscount);
-
-		totalAmount -= maxDiscount;
-		System.out.println("FinalAmount : " + totalAmount);
-
+		sst.totalAmount = order.getTotalAmount();
+		sst.orderMap = order.getOrderMap();
+		sst.totalProductDiscount = productPromos.getTotalProductDiscount(sst.orderMap);
+		System.out.println();
+		sst.netProductAmount = sst.totalAmount - sst.totalProductDiscount;
+		sst.totalOrderDiscount = orderPromo.getTotalOrderDiscount(sst.netProductAmount);
+		sst.printFinalReceipt();
 	}
-
 }
