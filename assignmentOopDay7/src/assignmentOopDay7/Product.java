4edb3291468d5819d63d7476e0a3f45afa434754
meta-ramvilas-jangid <ramvilas.jangid@metacube.com>
2017-07-27 08:28:04 +0530

    Update Product.java

--- assignmentOopDay7/src/assignmentOopDay7/Product.java
+++ assignmentOopDay7/src/assignmentOopDay7/Product.java
@@ -4,36 +4,49 @@ import java.util.StringTokenizer;
 
 public class Product {
 	private String id;
+	private String name;
+	/**
+	 * getters and setters
+	 */
+	private double price;
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	private String name;
-	private double price;
-	public Product(String productEntry){
-		StringTokenizer productEntryTokenizer = new StringTokenizer(productEntry,",");
+	/**
+	 * constructor to initialize product for every line read from file
+	 */
+	public Product(String productEntry) {
+		StringTokenizer productEntryTokenizer = new StringTokenizer(productEntry, ",");
 		id = productEntryTokenizer.nextToken();
 		name = productEntryTokenizer.nextToken();
 		price = Double.parseDouble(productEntryTokenizer.nextToken());
 	}
-	
-	public void displayProduct(){
-		System.out.print("id "+id);
-		System.out.print("   name "+name);
-		System.out.println("   price "+price);
+	/**
+	 * method to display a product
+	 */
+	public void displayProduct() {
+		System.out.println("product id\t\tproduct name\t\t\t\tproduct price");
+		System.out.println(id + "\t\t\t" + name + "\t\t\t\t" + price);
 	}
 }
