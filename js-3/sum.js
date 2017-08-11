eba910c59405f7516d9380f4b1c9cf4adf6954e8
meta-ramvilas-jangid <ramvilas.jangid@metacube.com>
2017-08-11 16:05:07 +0530

    first commit

---
+++ js-3/sum.js
@@ -0,0 +1,27 @@
+function sum(x,y) {
+    if (x && y){
+    	alert("h1");
+      document.getElementById('num1').innerHTML=x+y;
+    }
+    else {
+      return function(y) {
+      	alert("h2");x
+       document.getElementById('num2').innerHTML=x+y;
+       return x + y; 
+     };
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
